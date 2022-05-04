2a50116c6518581bc8d4bb1f67be1a519830a112
Stuart Thomas <stuart.thomas@fred.com.au>
2022-05-05 01:27:01 +1000

    allowed DOCKER_ROOT=$(pwd)

--- setup/plexdriveplus_install.sh
+++ setup/plexdriveplus_install.sh
@@ -4,7 +4,8 @@
 INSTALL_ENV_FILE=install.env
 [[ -z "$1" ]] || INSTALL_ENV_FILE=$1
 source $INSTALL_ENV_FILE
-[[ -z "$DOCKER_ROOT" ]] && DOCKER_ROOT=$(pwd) && echo "error: DOCKER_ROOT not set. using current path: $DOCKER_ROOT" && exit 1 || echo "Using DOCKER_ROOT path: $DOCKER_ROOT"
+([[ -z "$DOCKER_ROOT" ]] && DOCKER_ROOT=$(pwd) && echo "error: DOCKER_ROOT not set. using current path: $DOCKER_ROOT")
+echo "Using DOCKER_ROOT path: $DOCKER_ROOT"
 
 SUDO=sudo
 [[ $USER = "root" ]] && (echo "running as root user. wont use sudo " && SUDO=sudo)
