76c99ff33eb9daf55c969cc8d9527b841066483c
Stuart Thomas <stuart.thomas@fred.com.au>
2022-05-05 01:34:10 +1000

    prevented duplicate tar files

--- setup/pdp_install_helper.sh
+++ setup/pdp_install_helper.sh
@@ -1,4 +1,5 @@
 [[ $USER = "root" ]] && \
-curl -L  https://raw.githubusercontent.com/slink42/plexdriveplus/master/setup/plexdriveplus_install.sh -o plexdriveplus_install.sh && chmod +x ./plexdriveplus_install.sh && ./plexdriveplus_install.sh \
+(curl -L  https://raw.githubusercontent.com/slink42/plexdriveplus/master/setup/plexdriveplus_install.sh -o plexdriveplus_install.sh && chmod +x ./plexdriveplus_install.sh && ./plexdriveplus_install.sh || echo "install failed to complete")\
 || \
-sudo curl -L  https://raw.githubusercontent.com/slink42/plexdriveplus/master/setup/plexdriveplus_install.sh -o plexdriveplus_install.sh && sudo chmod +x ./plexdriveplus_install.sh && sudo ./plexdriveplus_install.sh
+sudo curl -L  https://raw.githubusercontent.com/slink42/plexdriveplus/master/setup/plexdriveplus_install.sh -o plexdriveplus_install.sh && sudo chmod +x ./plexdriveplus_install.sh && sudo ./plexdriveplus_install.sh \
+|| echo "install failed to complete"
