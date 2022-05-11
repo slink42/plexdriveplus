c3aff9600f607aa03e497aa1adc89836c685cc31
Stuart Thomas <stuart.thomas@fred.com.au>
2022-05-11 23:47:59 +1000

    Added support for multiple mounts

--- setup/plexdriveplus_install.sh
+++ setup/plexdriveplus_install.sh
@@ -92,7 +92,6 @@ tar xvzf "${DOCKER_ROOT}/plexdriveplus.tar.gz" --strip=1 -C "${DOCKER_ROOT}"
 echo "setting up rclone authentication"
 GDRIVE_ENDPOINT=$(cat $DOCKER_ROOT/config/.env | grep RCLONE_CONFIG_SECURE_MEDIA_REMOTE)
 GDRIVE_ENDPOINT=${GDRIVE_ENDPOINT/RCLONE_CONFIG_SECURE_MEDIA_REMOTE=/}
-# GDRIVE_ENDPOINT=$(cat $DOCKER_ROOT/config/.env | grep RCLONE_CONFIG_SECURE_MEDIA_REMOTE | cut -d "=" -f2)
 echo "Using rclone gdrive endpoint: $GDRIVE_ENDPOINT"
 if [[ -f "$DOCKER_ROOT/rclone/rclone.conf" ]] && [[ $(rclone --config  "$DOCKER_ROOT/rclone/rclone.conf" lsd $GDRIVE_ENDPOINT) ]]; then 
 	echo "rclone auth already present. Skipping config copy from master copy mount reconnection"
@@ -102,6 +101,29 @@ else
     echo "reconnecting rclone mount: $GDRIVE_ENDPOINT"
 	rclone config --config "$DOCKER_ROOT/rclone/rclone.conf" reconnect $GDRIVE_ENDPOINT
 fi
+
+# authorize scanner rclone gdrive mount if required by selected library managemeent mode
+if [[ $management_mode = "2" ]] || [[ $management_mode = "3" ]]; then
+    echo "setting up rclone authentication form library scanner mount"
+    SCANNER_GDRIVE_ENDPOINT=$(cat $DOCKER_ROOT/config/.env | grep RCLONE_CONFIG_SECURE_MEDIA_SCANNER_REMOTE)
+    SCANNER_GDRIVE_ENDPOINT=${SCANNER_GDRIVE_ENDPOINT/RCLONE_CONFIG_SECURE_MEDIA_SCANNER_REMOTE=/}
+    echo "Using rclone gdrive endpoint for scanner: $SCANNER_GDRIVE_ENDPOINT"
+    if [[ $(rclone --config  "$DOCKER_ROOT/rclone/rclone.conf" lsd $SCANNER_GDRIVE_ENDPOINT) ]]; then 
+        echo "rclone auth already present. Skipping config copy from master copy mount reconnection"
+    else
+        echo "rclone onfig copy from master $SCANNER_GDRIVE_ENDPOINT mount reconnection"
+        rclone config --config "$DOCKER_ROOT/rclone/rclone.conf" reconnect $SCANNER_GDRIVE_ENDPOINT
+    fi
+    fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media_scanner 2>/dev/null
+    fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media2_scanner 2>/dev/null
+    fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media3_scanner 2>/dev/null
+fi
+
+# make sure paths aren't mounted
+fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media 2>/dev/null
+fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media2 2>/dev/null
+fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media3 2>/dev/null
+fusermount -uz $DOCKER_ROOT/mnt/plexdrive/secure_media 2>/dev/null
 
 ## copy gdrive mount tokens to plexdrive
 echo "copying rclone token to plexdrive"
@@ -115,6 +137,7 @@ RCLONE_TOKEN=$(echo "$RCLONE_CONFIG_GDRIVE" | grep token)
 RCLONE_TOKEN=${RCLONE_TOKEN/token = /}
 [[ -z "$RCLONE_TOKEN" ]] && echo "error: rclone token for gdrive not found in rclone.conf" && exit 1 \
     || echo "$RCLONE_TOKEN" > "$DOCKER_ROOT/plexdrive/config/token.json"
+
 
 # update plexdrive config.json
 RCLONE_CLIENTID=$(echo "$RCLONE_CONFIG_GDRIVE" | grep client_id)
@@ -136,8 +159,6 @@ RCLONE_TEAMDRIVE=${RCLONE_TEAMDRIVE/team_drive = /}
 [[ -z "$RCLONE_TEAMDRIVE" ]] && echo "warning: rclone teamdrive id for gdrive not found in rclone.conf" \
     || echo "$RCLONE_TEAMDRIVE" > "$DOCKER_ROOT/plexdrive/config/team_drive.id"
 
-GDRIVE_ENDPOINT=$(cat $DOCKER_ROOT/config/.env | grep RCLONE_CONFIG_SECURE_MEDIA_REMOTE)
-GDRIVE_ENDPOINT=${GDRIVE_ENDPOINT/RCLONE_CONFIG_SECURE_MEDIA_REMOTE=/}
 
 # Download plexdrive cache
 if [[ -z "$USE_CLOUD_CONFIG" ]] && [[ -f "$DOCKER_ROOT/plexdrive/cache/cache.bolt" ]]; then
@@ -156,35 +177,15 @@ else
     fi
 fi
 
+# Set rclone rc username and password if not already provided in env file
+[[ $(cat $ENV_FILE | grep RCLONE_USER) ]] || echo "RCLONE_USER=rclone" >> "$ENV_FILE"
+[[ $(cat $ENV_FILE | grep RCLONE_PASSWORD) ]] || echo "RCLONE_PASSWORD=rclone" >> "$ENV_FILE"
+
 ## Start with updated rclone config
 echo "starting containers with docker-compose"
 ENV_FILE="$DOCKER_ROOT/config/.env"
 sed -i '/DOCKER_ROOT/'d "$ENV_FILE"
 echo "DOCKER_ROOT=$DOCKER_ROOT" >> "$ENV_FILE"
-
-# Set rclone rc username and password if not already provided in env file
-[[ $(cat $ENV_FILE | grep RCLONE_USER) ]] || echo "RCLONE_USER=rclone" >> "$ENV_FILE"
-[[ $(cat $ENV_FILE | grep RCLONE_PASSWORD) ]] || echo "RCLONE_PASSWORD=rclone" >> "$ENV_FILE"
-
-# authorize scanner rclone gdrive mount if required by selected library managemeent mode
-if [[ $management_mode = "2" ]] || [[ $management_mode = "3" ]]; then
-    echo "setting up rclone authentication form library scanner mount"
-    SCANNER_GDRIVE_ENDPOINT=$(cat $DOCKER_ROOT/config/.env | grep RCLONE_CONFIG_SECURE_MEDIA_SCANNER_REMOTE)
-    SCANNER_GDRIVE_ENDPOINT=${GDRIVE_ENDPOINT/RCLONE_CONFIG_SECURE_MEDIA_SCANNER_REMOTE=/}
-    # GDRIVE_ENDPOINT=$(cat $DOCKER_ROOT/config/.env | grep RCLONE_CONFIG_SECURE_MEDIA_REMOTE | cut -d "=" -f2)
-    echo "Using rclone gdrive endpoint for scanner: $SCANNER_GDRIVE_ENDPOINT"
-    if [[ $(rclone --config  "$DOCKER_ROOT/rclone/rclone.conf" lsd $SCANNER_GDRIVE_ENDPOINT) ]]; then 
-        echo "rclone auth already present. Skipping config copy from master copy mount reconnection"
-    else
-        echo "rclone onfig copy from master $SCANNER_GDRIVE_ENDPOINT mount reconnection"
-        rclone config --config "$DOCKER_ROOT/rclone/rclone.conf" reconnect $SCANNER_GDRIVE_ENDPOINT
-    fi
-    fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media_scanner
-fi
-
-# make sure paths aren't mounted
-fusermount -uz $DOCKER_ROOT/mnt/rclone/secure_media 2>/dev/null
-fusermount -uz $DOCKER_ROOT/mnt/plexdrive/secure_media 2>/dev/null
 
 # start docker containers
 DOCKER_COMPOSE_FILE=$DOCKER_ROOT/setup/docker-compose.yml
