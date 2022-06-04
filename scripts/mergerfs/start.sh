dda26123c8e89fcf9c09ea547b0725f539313795
Stuart Thomas <stuart.thomas@fred.com.au>
2022-06-04 17:02:00 +1000

    custom mergerfs startup

---
+++ scripts/mergerfs/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+[ -z $MFS_USER_OPTS ] && MFS_USER_OPTS="async_read=false,use_ino,allow_other,func.getattr=newest,category.action=all,category.create=ff,cache.files=partial,dropcacheonclose=true"
+[ -z $SOURCE_PATHS ] && SOURCE_PATHS="/mnt/plexdrive/secure_media/=RO:/mnt/rclone/secure_media=RO:/mnt/rclone/secure_media2=RO:/mnt/rclone/secure_media3=RO"
+[ -z $DEST_PATH ] && DEST_PATH="/data/media"
+
+#  - -o
+#             - allow_other # allow access to other users
+#             - -o
+#             - auto_cache # enable caching based on modification times
+#             - -o
+#             - auto_unmount # auto unmount on process termination
+#             - -o
+#             - direct_io # use direct I/O
+#             - -o
+#             - gid=${GROUPID} # set file group
+#             - -o
+#             - sync_read # perform reads synchronously
+#             - -o
+#             - uid=${USERID} # set file owner
+#             - /mnt/plexdrive/secure_media/=RO:/mnt/rclone/secure_media=RO:/mnt/rclone/secure_media2=RO:/mnt/rclone/secure_media3=RO # source paths
+#             - /data/media # mergerfs mounts
+
+# make sure old path is unmounted
+fusermount -uz "$DEST_PATH"
+# start mergerfss mount
+mergerfs -f -o "$MFS_USER_OPTS" "$SOURCE_PATHS" "$DEST_PATH"
