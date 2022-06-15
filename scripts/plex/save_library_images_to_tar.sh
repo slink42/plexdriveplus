acd3e4614e32c726a3a84f3408a74fec709ceec5
Stuart Thomas <stuart.thomas@fred.com.au>
2022-06-15 18:52:13 +1000

    added script for making tar of image files

---
+++ scripts/plex/save_library_images_to_tar.sh
@@ -0,0 +1,19 @@
+PLEX_LIBRARY_ROOT="/config/Library"
+LOG_FILE=/config/backups/meta/library_files_updated.tar.gz.log
+
+echo "$(date) ****** Starting image Libary tar file rebuild ******"
+echo "$(date) ****** Starting image Libary tar file rebuild ******" > "$LOG_FILE"
+cd "$PLEX_LIBRARY_ROOT"
+tar -cvpzf /config/backups/meta/library_files_updated.tar.gz "./Application Support/Plex Media Server/Metadata" "./Application Support/Plex Media Server/Media" >> "$LOG_FILE"
+
+if [ $(gzip -t /config/backups/meta/library_files_updated.tar.gz) ]
+then
+    echo "tar gzip compression tested ok, overwriting old version"
+    # mv /config/backups/meta/library_files_updated.tar.gz /config/backups/meta/library_files.tar.gz
+else
+    echo "error tar gzip compression failed when tested, removing instead of overwriting old version"
+    rm /config/backups/meta/library_files_updated.tar.gz
+fi
+
+echo "$(date) ****** Finished image Libary tar file rebuild ******" >> "$LOG_FILE"
+echo "$(date) ****** Finished image Libary tar file rebuild ******"
